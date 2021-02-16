72bb339c13cd8506e7956eebf71795ace38d1187
Yerem Sargsyan <eresar01@gmial.com>
2021-02-16 22:27:45 +0400

    first commit

---
+++ OurMusic/OurMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OurMusic
+//
+//  Created by Yerem Sargsyan on 16.02.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
