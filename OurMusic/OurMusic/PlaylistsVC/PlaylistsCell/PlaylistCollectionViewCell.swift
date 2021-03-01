a48cf7bcb5ae150c993f53f54bf6d44630270aeb
Areg <arehov88@gmail.com>
2021-03-01 16:58:33 +0400

    creat playlistVC

---
+++ OurMusic/OurMusic/PlaylistsVC/PlaylistsCell/PlaylistCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  PlaylistCollectionViewCell.swift
+//  OurMusic
+//
+//  Created by Areg on 2/23/21.
+//
+
+import UIKit
+
+class PlaylistCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var firstImage: UIImageView!
+    @IBOutlet weak var secondImage: UIImageView!
+    @IBOutlet weak var thirdImage: UIImageView!
+    @IBOutlet weak var fourthImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
